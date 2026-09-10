dcbe8bf790250e729d2c4ec2ad155ae111363d97
shilhun1 <20260198@vision.hoseo.edu>
2026-09-10 16:50:30 +0900

    .

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -1,20 +1,83 @@
-﻿// GameEngine.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <Windows.h>
+#include <d3d11.h>
 
-#include <iostream>
 
-int main()
+LRESULT CALLBACK WindowProc(
+	HWND hwnd,
+	UINT message,
+	WPARAM wParam,
+	LPARAM lParam)
 {
-    std::cout << "Hello World!\n";
+	switch (message)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+
+		return 0;
+	}
+	return DefWindowProc(
+		hwnd,
+		message,
+		wParam,
+		lParam
+	);
 }
+int WINAPI WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow)
+{
+	const char CLASS_NAME[] = "MyWindow";
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+	WNDCLASSA wc = {};
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = CLASS_NAME;
+
+	wc.hbrBackground =
+		CreateSolidBrush(RGB(50, 100, 200));
+
+	RegisterClassA(&wc);
+
+	int width = 1200;
+	int height = 800;
+
+	HWND hwnd = CreateWindowExA(
+		0,
+		CLASS_NAME,
+		"My Engine",
+
+		WS_OVERLAPPEDWINDOW,
+
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+
+		width,
+		height,
+
+		nullptr,
+		nullptr,
+
+		hInstance,
+		nullptr
+	);
+
+	if (hwnd == nullptr)
+	{
+		return 0;
+	}
+
+	ShowWindow(hwnd, nCmdShow);
+
+	MSG msg = {};
+
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessageA(&msg);
+	}
+
+	return 0;
+}
